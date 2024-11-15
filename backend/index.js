bc8727b9a2d2251f46a0154e6e85a7eb96a685ff
Reeyas13 <reeyaskarki@gmail.com>
2024-11-15 19:37:52 +0545

    conect to vercel

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ import app from "./utils/express.js";
 import path from "path";
 import express from "express";
 import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+// app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build/", "index.html"));
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/client/build/", "index.html"));
+// })
 
 
 app.listen(process.env.APP_PORT||3000, () => {
